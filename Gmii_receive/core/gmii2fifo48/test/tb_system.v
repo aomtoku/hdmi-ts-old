03336432b786da332db0c873c4aabb68030bb0ad
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-11-18 03:35:48 +0900

    testbench_gmii2fifo24: 1280 x 720 x 2 frame check scenario

--- Gmii_receive/core/gmii2fifo48/test/tb_system.v
+++ Gmii_receive/core/gmii2fifo48/test/tb_system.v
@@ -15,6 +15,7 @@ always #8 sys_clk = ~sys_clk;
 // Test Bench
 //
 reg sys_rst;
+reg eof;
 reg rx_dv;
 reg [7:0] rxd;
 wire [28:0] datain;
@@ -42,15 +43,58 @@ end
 endtask
 
 //
+// Check FIFO
+//
+reg [11:0] check_y = 12'd00, check_x = 12'd0;
+wire [11:0] org_x, org_y;
+assign org_x = (datain[15:0] - 4096);
+assign org_y = {1'b0,datain[26:16]};
+
+always @(posedge sys_clk) begin
+	if (sys_rst) begin
+	end else if (recv_en == 1'b1) begin
+		if (datain[27] == 1'b0 &&  org_x >= 640) begin
+			$display( "error type 0" );
+			$display( " at ", check_x,",", check_y, "   data:" , org_x,",", org_y );
+		end
+		if (datain[27] == 1'b1 &&  org_x  < 640) begin
+			$display( "error type 1" );
+			$display( " at ", check_x,",", check_y, "   data:" , org_x,",", org_y );
+		end
+		if (org_x != check_x) begin
+			$display( "error type 2" );
+			$display( " at ", check_x,",", check_y, "   data:" , org_x,",", org_y );
+		end
+		if (org_y != check_y) begin
+			$display( "error type 3" );
+			$display( " at ", check_x,",", check_y, "   data:" , org_x,",", org_y );
+		end
+		if ( check_x != 12'd1279) begin
+			check_x <= check_x + 12'd1;
+		end else begin
+			check_x <= 12'd0;
+			if ( check_y != 12'd719) begin
+				check_y <= check_y + 12'd1;
+			end else begin
+				check_y <= 12'd0;
+			end
+		end
+	end
+end
+
+//
 // Scinario
 //
 
-reg [8:0] rom [0:65535];
-reg [15:0] counter = 16'd0;
+reg [9:0] rom [0:3890000];
+reg [23:0] counter = 24'd0;
 
 always @(posedge sys_clk) begin
-	{rx_dv, rxd} 	<= rom[counter];
-	counter		<= counter + 16'd1;
+	{eof, rx_dv, rxd} <= rom[counter];
+	if (eof) begin
+		$finish;
+	end
+	counter		<= counter + 24'd1;
 end
 
 
@@ -69,8 +113,8 @@ initial begin
 	waitclock;
 	
 	
-	#1000000;
-	$finish;
+//	#1000000;
+//	$finish;
 end
 
 endmodule
