770ceafa5bd10807ef9de211c138e2287afbb491
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-11-18 21:48:05 +0900

    fixed HUV to RGB

--- Gmii_receive/core/common/datacontroller.v
+++ Gmii_receive/core/common/datacontroller.v
@@ -8,13 +8,13 @@ module datacontroller # (
   input wire [11:0] i_hcnt, //horizontal counter from video timing generator
   output wire  fifo_read,
   input wire [28:0] data,
-  input wire 		  sw,
+  input wire        sw,
   
-  output [7:0]  o_r,
-  output [7:0]  o_g,
-  output [7:0]  o_b
+  output wire [7:0]  o_r,
+  output wire [7:0]  o_g,
+  output wire [7:0]  o_b
 );
-wire [1:0] x_count;
+wire [1:0]  x_count;
 wire [10:0] y_count;
 assign x_count = data[28:27];
 assign y_count = data[26:16];
@@ -55,14 +55,18 @@ parameter vfin	 = 12'd745;
 reg hactive,vactive;
 reg xblock;
 reg [18:0] Y, Cb, Cr, a_r, a_g, a_b;
+reg [7:0] b_r, b_g, b_b;
 
 always @ (posedge i_clk_74M) begin
 	if(i_rst) begin
 	    hactive  <= 1'b0;
 	    vactive  <= 1'b0;
-            a_r      <= 8'h00;
-            a_g      <= 8'h00;
-            a_b      <= 8'h00;
+            a_r      <= 12'h00;
+            a_g      <= 12'h00;
+            a_b      <= 12'h00;
+            b_r      <= 8'h00;
+            b_g      <= 8'h00;
+            b_b      <= 8'h00;
 	    xblock   <= 1'b0;
 	end else begin
 			if(i_hcnt == hstart) begin
@@ -90,30 +94,33 @@ always @ (posedge i_clk_74M) begin
 						a_r <= ( (Y<<8) + (19'b1_0110_0111*Cr) - 19'hb380)>>8;
 						a_g <= ( (Y<<8) + 19'h8780 - (19'b1011_0111*Cr) - (19'b0101_1000*Cb) )>>8;
 						a_b <= ( (Y<<8) + (19'b1_1100_0110*Cb) - 19'he300)>>8;
+						b_r <= (a_r >= 19'hff) ? 8'hff : a_r[7:0];
+						b_g <= (a_g >= 19'hff) ? 8'hff : a_g[7:0];
+						b_b <= (a_b >= 19'hff) ? 8'hff : a_b[7:0];
 //						a_b <= data[7:0];
 //						a_g <= data[15:8];
 //						a_r <= 8'b0;
 					end else begin
-						a_b <= 8'h0;
-						a_g <= 8'h0;
-						a_r <= 8'h0;
+						b_b <= 8'h0;
+						b_g <= 8'h0;
+						b_r <= 8'h0;
 					end
 				end else begin
-					a_b <= i_hcnt[9:2];
-					a_g <= i_vcnt[8:1];
-					a_r <= 8'h0;
+					b_b <= i_hcnt[9:2];
+					b_g <= i_vcnt[8:1];
+					b_r <= 8'h0;
 				end
 			end else begin
-				a_b <= 8'h00;
-				a_g <= 8'h00;
-				a_r <= 8'h00;
+				b_b <= 8'h00;
+				b_g <= 8'h00;
+				b_r <= 8'h00;
 			end
 	end
 end
 
 assign fifo_read = (hactive & vactive);
-assign o_r = a_r[7:0];
-assign o_g = a_g[7:0];
-assign o_b = a_b[7:0];
+assign o_r = b_r[7:0];
+assign o_g = b_g[7:0];
+assign o_b = b_b[7:0];
 
 endmodule
