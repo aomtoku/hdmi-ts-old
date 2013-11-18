e3009e09d1c1f7d1ab9bfd61ac1a89bb08864cbd
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-11-18 19:59:40 +0900

    conversion YUB to TGB

--- Gmii_receive/core/common/datacontroller.v
+++ Gmii_receive/core/common/datacontroller.v
@@ -10,20 +10,20 @@ module datacontroller # (
   input wire [28:0] data,
   input wire 		  sw,
   
-  output reg [7:0]  o_r,
-  output reg [7:0]  o_g,
-  output reg [7:0]  o_b
+  output [7:0]  o_r,
+  output [7:0]  o_g,
+  output [7:0]  o_b
 );
 wire [1:0] x_count;
 wire [10:0] y_count;
 assign x_count = data[28:27];
 assign y_count = data[26:16];
 
-//`define CLIP(X) ( (X) > 255 ? 255 : (X) < 0 ? 0 : X)
+//`define CLIP(X) ( (X) > 16'd255 ? 16'd255 : (X) < 16'd0 ? 16'd0 : X)
 // YCbCr -> RGB
-//`define CYCbCr2R(Y, Cb, Cr) CLIP( Y + ( 91881 * Cr >> 16 ) - 179 )
-//`define CYCbCr2G(Y, Cb, Cr) CLIP( Y - (( 22544 * Cb + 46793 * Cr ) >> 16) + 135)
 //`define CYCbCr2B(Y, Cb, Cr) CLIP( Y + (116129 * Cb >> 16 ) - 226 )
+//`define CYCbCr2G(Y, Cb, Cr) CLIP( Y - (( 22544 * Cb + 46793 * Cr ) >> 16) + 135)
+//`define CYCbCr2R(Y, Cb, Cr) CLIP( Y + ( 91881 * Cr >> 16 ) - 179 )
 
 
 /*
@@ -54,14 +54,15 @@ parameter vfin	 = 12'd745;
 
 reg hactive,vactive;
 reg xblock;
+reg [18:0] Y, Cb, Cr, a_r, a_g, a_b;
 
 always @ (posedge i_clk_74M) begin
 	if(i_rst) begin
 	    hactive  <= 1'b0;
 	    vactive  <= 1'b0;
-            o_r      <= 8'h00;
-            o_g      <= 8'h00;
-            o_b      <= 8'h00;
+            a_r      <= 8'h00;
+            a_g      <= 8'h00;
+            a_b      <= 8'h00;
 	    xblock   <= 1'b0;
 	end else begin
 			if(i_hcnt == hstart) begin
@@ -79,29 +80,40 @@ always @ (posedge i_clk_74M) begin
 			if(i_vcnt == vfin) 
 					vactive <= 1'b0;
 			if (hactive & vactive) begin
+				Y <= {11'b0,data[15:8]};
+				if (i_hcnt[0] == 1'b0)
+					Cr <= {11'b0, data[7:0]};
+				else
+					Cb <= {11'b0, data[7:0]};
 				if(sw)begin
 					if (x_count[0] == xblock) begin
-						o_b <= data[7:0];
-						o_g <= data[15:8];
-						o_r <= 8'b0;
+						a_r <= ( (Y<<8) + (19'd359*Cr) - 19'd45952)>>8;
+						a_g <= ((Y<<8) + 19'd34688 - 19'd183*Cr - 19'd88*Cb )>>8;
+						a_b <= ( (Y << 8) + (19'd454*Cb) - 19'd58112)>>8;
+//						a_b <= data[7:0];
+//						a_g <= data[15:8];
+//						a_r <= 8'b0;
 					end else begin
-						o_b <= 8'h0;
-						o_g <= 8'h0;
-						o_r <= 8'h0;
+						a_b <= 8'h0;
+						a_g <= 8'h0;
+						a_r <= 8'h0;
 					end
 				end else begin
-					o_b <= i_hcnt[9:2];
-					o_g <= i_vcnt[8:1];
-					o_r <= 8'h0;
+					a_b <= i_hcnt[9:2];
+					a_g <= i_vcnt[8:1];
+					a_r <= 8'h0;
 				end
 			end else begin
-				o_b <= 8'h00;
-				o_g <= 8'h00;
-				o_r <= 8'h00;
+				a_b <= 8'h00;
+				a_g <= 8'h00;
+				a_r <= 8'h00;
 			end
 	end
 end
 
 assign fifo_read = (hactive & vactive);
+assign o_r = a_r[7:0];
+assign o_g = a_g[7:0];
+assign o_b = a_b[7:0];
 
 endmodule
