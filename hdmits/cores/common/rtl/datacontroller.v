389d8bd5d99d7f6dae584f76638706c58deb9c69
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2013-11-26 18:20:19 +0900

    fixed

--- hdmits/cores/common/rtl/datacontroller.v
+++ hdmits/cores/common/rtl/datacontroller.v
@@ -42,7 +42,7 @@ parameter vfin	 = 12'd745;
 `endif
 
 reg hactive,vactive;
-//reg xblock;
+reg xblock;
 reg [18:0] Y, Cb, Cr, a_r, a_g, a_b;
 reg [7:0] b_r, b_g, b_b;
 
@@ -56,15 +56,15 @@ always @ (posedge i_clk_74M) begin
 		b_r      <= 8'h00;
 		b_g      <= 8'h00;
 		b_b      <= 8'h00;
-//		xblock   <= 1'b0;
+		xblock   <= 1'b0;
 	end else begin
 		if(i_hcnt == hstart) begin
 			hactive <= 1'b1;
-//			xblock  <= 1'b0;
+			xblock  <= 1'b0;
 		end
 
-//		if(i_hcnt == (hstart + 641))
-//			xblock  <= 1'b1;
+		if(i_hcnt == (hstart + 641))
+			xblock  <= 1'b1;
 
 		if(i_hcnt == hfin) 
 			hactive <= 1'b0;
@@ -82,18 +82,18 @@ always @ (posedge i_clk_74M) begin
 			else
 				Cb <= {11'b0, data[7:0]};
 			if (sw) begin
-//				if (x_count[0] == xblock) begin
+				if (x_count[0] == xblock) begin
 					a_r <= ( (Y<<8) + (19'b1_0110_0111*Cr) - 19'hb380)>>8;
 					a_g <= ( (Y<<8) + 19'h8780 - (19'b1011_0111*Cr) - (19'b0101_1000*Cb) )>>8;
 					a_b <= ( (Y<<8) + (19'b1_1100_0110*Cb) - 19'he300)>>8;
 					b_r <= (a_r >= 19'hff) ? 8'hff : a_r[7:0];
 					b_g <= (a_g >= 19'hff) ? 8'hff : a_g[7:0];
 					b_b <= (a_b >= 19'hff) ? 8'hff : a_b[7:0];
-//				end else begin
-//					b_b <= 8'h0;
-//					b_g <= 8'h0;
-//					b_r <= 8'h0;
-//				end
+				end else begin
+					b_b <= 8'h0;
+					b_g <= 8'h0;
+					b_r <= 8'h0;
+				end
 			end else begin
 				b_b <= i_hcnt[9:2];
 				b_g <= i_vcnt[8:1];
