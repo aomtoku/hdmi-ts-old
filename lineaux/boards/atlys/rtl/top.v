ef638574db5f802c34801e9671ba8f0808b9ba79
aom <aom@n128-107.sfc.wide.ad.jp>
2014-03-05 02:55:33 +0900

    mod

--- lineaux/boards/atlys/rtl/top.v
+++ lineaux/boards/atlys/rtl/top.v
@@ -230,11 +230,11 @@ always @ (posedge rx0_pclk) begin
 	else
 		hsycnt <= 8'd0;
 
-	if(rx0_ade && (hcnt >= 11'd1521) && (hcnt <= 11'd1530))
+	if(rx0_ade && (hcnt == 11'd1511) /*&& (hcnt <= 11'd1530)*/)
      ap <= 1'b1;
-	if(rx0_ade && (hcnt >= 11'd1515) && (hcnt <= 11'd1521))
+	if(rx0_ade && (hcnt >= 11'd1514) && (hcnt <= 11'd1515))
      apb <= 1'b1;
-	if(rx0_ade && (hcnt >= 11'd1511) && (hcnt <= 11'd1515) )
+	if(rx0_ade && (hcnt >= 11'd1512) && (hcnt <= 11'd1513) )
      apa <= 1'b1;
 end
 
