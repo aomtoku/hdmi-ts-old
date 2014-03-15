e453d229b1493c0216aa25fbd5f76d2978ea584f
aom <aom@aom.sfc.wide.ad.jp>
2014-03-16 05:06:48 +0900

    modefied

--- hdmits-cmp/cores/adpcm/rtl/dc_adpcm.v
+++ hdmits-cmp/cores/adpcm/rtl/dc_adpcm.v
@@ -17,10 +17,6 @@ reg [15:0] bp;
 reg [15:0] y,cbcr;
 reg [15:0] out;
 reg        oen;
-wire [7:0] yin  = din[7:0 ]; 
-wire [7:0] rbin = din[15:8];
-wire [7:0] ybp  = bp [7:0 ];
-wire [7:0] rbbp = bp [15:8];
 
 wire [15:0]yout = (~eo) ? {cbcr[7:0], y[7:0]} : out;
 
@@ -32,8 +28,6 @@ always@(posedge clk)
 		eo <= 1'b0;
 	else
 		eo <= ~eo;
-
-
 
 always@(posedge clk)begin
   if(rst)begin
