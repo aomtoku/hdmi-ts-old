6f5877c9f4b041816340ac3d79979ae021125b53
aom <aom@aom.sfc.wide.ad.jp>
2014-03-30 20:54:40 +0900

    m

--- ethaudio/boards/atlys/rtl/top.v
+++ ethaudio/boards/atlys/rtl/top.v
@@ -1111,7 +1111,7 @@ always @(RXCLK) begin
 		//1'b0 : LED <= {anum,recv_full,recv_empty,ax_rx_rd_en,vde};
 		//1'b1 : LED <= {ax_recv_full,ax_recv_empty,ax_recv_wr_en,ax_recv_rd_en,ax_send_full,ax_send_empty,ax_send_wr_en,ax_send_rd_en};
 		1'b0 : LED <= {send_full, send_empty, ax_send_full, ax_send_empty,ax_rx_rd_en,ax_send_wr_en,ax_recv_full,ax_recv_empty};
-		1'b1 : LED <= axdout[7:0];
+		1'b1 : LED <= {6'd0,ade_m,rx0_ade};
 		//4'b1000 : LED <= {4'b0,recv_full,recv_empty,2'b0};
 		//4'b0001 : LED <= error[7:0];
 		//4'b0010 : LED <= {5'd0,error[10:8]};
