c8a4fb722dfe6a70b0fcecd1700e4758d32669f4
aom <aom@aom.sfc.wide.ad.jp>
2014-04-23 00:57:36 +0900

    adding wishbone

---
+++ cputest/test2/wishbonetest/ipcore_dir/mb_mcs_readme.txt
@@ -0,0 +1,78 @@
+The following files were generated for 'mb_mcs' in directory
+/home/aom/Work/fpga_project/AtlysProjectAom/cputest/test2/wishbonetest/ipcore_dir/
+
+Generate XCO file:
+   CORE Generator input file containing the parameters used to generate a core.
+
+   * mb_mcs.xco
+
+Generate Implementation Netlist:
+   Binary Xilinx implementation netlist files containing the information
+   required to implement the module in a Xilinx (R) FPGA.
+
+   * mb_mcs.ngc
+
+Misc Files Generator:
+   Please see the core data sheet.
+
+   * mb_mcs/mb_bootloop_le.elf
+   * mb_mcs/microblaze_mcs_setup.tcl
+   * mb_mcs/system_template.tcl
+
+Generate Script:
+   Execute microblaze_mcs_gen_script.tcl for generating bmm files and SDK HW
+   import file.
+
+   * mb_bootloop_le.elf
+   * mb_mcs.bmm
+   * mb_mcs_sdk.xml
+   * microblaze_mcs_setup.tcl
+
+Generate Instantiation Templates:
+   Template files containing code that can be used as a model for instantiating
+   a CORE Generator module in an HDL design.
+
+   * mb_mcs.veo
+
+RTL Simulation Model Generator:
+   Please see the core data sheet.
+
+   * mb_mcs.v
+
+Simulation Netlist Update Script:
+   Execute microblaze_mcs_sim_script.tcl to add INIT_FILE filenames to
+   simulation netlist.
+
+   * mb_mcs.v
+
+Deliver IP Symbol:
+   Graphical symbol information file. Used by the ISE tools and some third party
+   tools to create a symbol representing the core.
+
+   * mb_mcs.asy
+
+SYM file generator:
+   Generate a SYM file for compatibility with legacy flows
+
+   * mb_mcs.sym
+
+Synthesis ISE Generator:
+   Please see the core data sheet.
+
+   * mb_mcs.gise
+   * mb_mcs.xise
+
+Deliver Readme:
+   Readme file for the IP.
+
+   * mb_mcs_readme.txt
+
+Generate FLIST file:
+   Text file listing all of the output files produced when a customized core was
+   generated in the CORE Generator.
+
+   * mb_mcs_flist.txt
+
+Please see the Xilinx CORE Generator online help for further details on
+generated files and how to use them.
+
---
+++ cputest/test2/wishbonetest/top.v
@@ -0,0 +1,106 @@
+/*******************************************************************************
+*     This file is owned and controlled by Xilinx and must be used solely      *
+*     for design, simulation, implementation and creation of design files      *
+*     limited to Xilinx devices or technologies. Use with non-Xilinx           *
+*     devices or technologies is expressly prohibited and immediately          *
+*     terminates your license.                                                 *
+*                                                                              *
+*     XILINX IS PROVIDING THIS DESIGN, CODE, OR INFORMATION "AS IS" SOLELY     *
+*     FOR USE IN DEVELOPING PROGRAMS AND SOLUTIONS FOR XILINX DEVICES.  BY     *
+*     PROVIDING THIS DESIGN, CODE, OR INFORMATION AS ONE POSSIBLE              *
+*     IMPLEMENTATION OF THIS FEATURE, APPLICATION OR STANDARD, XILINX IS       *
+*     MAKING NO REPRESENTATION THAT THIS IMPLEMENTATION IS FREE FROM ANY       *
+*     CLAIMS OF INFRINGEMENT, AND YOU ARE RESPONSIBLE FOR OBTAINING ANY        *
+*     RIGHTS YOU MAY REQUIRE FOR YOUR IMPLEMENTATION.  XILINX EXPRESSLY        *
+*     DISCLAIMS ANY WARRANTY WHATSOEVER WITH RESPECT TO THE ADEQUACY OF THE    *
+*     IMPLEMENTATION, INCLUDING BUT NOT LIMITED TO ANY WARRANTIES OR           *
+*     REPRESENTATIONS THAT THIS IMPLEMENTATION IS FREE FROM CLAIMS OF          *
+*     INFRINGEMENT, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A    *
+*     PARTICULAR PURPOSE.                                                      *
+*                                                                              *
+*     Xilinx products are not intended for use in life support appliances,     *
+*     devices, or systems.  Use in such applications are expressly             *
+*     prohibited.                                                              *
+*                                                                              *
+*     (c) Copyright 1995-2014 Xilinx, Inc.                                     *
+*     All rights reserved.                                                     *
+*******************************************************************************/
+
+/*******************************************************************************
+*     Generated from core with identifier: xilinx.com:ip:microblaze_mcs:1.3    *
+*                                                                              *
+*     MicroBlaze Micro Controller System (MCS) is a light-weight general       *
+*     purpose micro controller system, based on the MicroBlaze processor.      *
+*     It is primarily intended for simple control applications, where a        *
+*     hardware solution would be less flexible and more difficult to           *
+*     implement. Software development with the Xilinx Software Development     *
+*     Kit (SDK) is supported, including a software driver for the              *
+*     peripherals. Debugging is available either via SDK or directly with      *
+*     the Xilinx Microprocessor Debugger.                                      *
+*                                                                              *
+*     The MCS consists of the processor itself, local memory with sizes        *
+*     ranging from 4KB to 64KB, up to 4 Fixed Interval Timers, up to 4         *
+*     Programmable Interval Timers, up to 4 32-bit General Purpose Output      *
+*     ports, up to 4 32-bit General Purpose Input ports, and an Interrupt      *
+*     Controller with up to 16 external interrupt inputs.                      *
+*                                                                              *
+*******************************************************************************/
+module top(
+  input  wire       Clk,
+	input  wire       Reset,
+  input  wire       UART_Rx,
+	output wire       UART_Tx,
+	output wire [3:0] led,
+  input  wire [3:0] dip_sw
+);
+// Interfaces:
+//    IO_BUS
+//        MicroBlaze MCS IO Bus Interface
+//    TRACE
+//        MicroBlaze MCS Trace Interface
+
+ wire IO_Addr_Strobe,IO_Read_Strobe;
+ wire IO_Write_Strobe;
+ wire [31:0] IO_Address;
+ wire [ 3:0] IO_Byte_Enable;
+ wire [31:0] IO_Write_Data, IO_Read_Data;
+ wire IO_Ready;
+ wire FIT1_Interrupt, PIT1_Interrupt;
+ wire FIT1_Toggle, PIT1_Toggle;
+ wire [31:0]GPO1, GPI1;
+ wire GPI1_Interrupt;
+ wire INTC_Interrupt;
+ wire INTC_IRQ;
+
+ assign led = GPO1[3:0];
+
+mb_mcs mcs_0 (
+  .Clk(Clk), // input Clk
+  .Reset(Reset), // input Reset
+  .IO_Addr_Strobe(IO_Addr_Strobe), // output IO_Addr_Strobe
+  .IO_Read_Strobe(IO_Read_Strobe), // output IO_Read_Strobe
+  .IO_Write_Strobe(IO_Write_Strobe), // output IO_Write_Strobe
+  .IO_Address(IO_Address), // output [31 : 0] IO_Address
+  .IO_Byte_Enable(IO_Byte_Enable), // output [3 : 0] IO_Byte_Enable
+  .IO_Write_Data(IO_Write_Data), // output [31 : 0] IO_Write_Data
+  .IO_Read_Data(IO_Read_Data), // input [31 : 0] IO_Read_Data
+  .IO_Ready(IO_Ready), // input IO_Ready
+  .UART_Rx(UART_Rx), // input UART_Rx
+  .UART_Tx(UART_Tx), // output UART_Tx
+  .FIT1_Interrupt(FIT1_Interrupt), // output FIT1_Interrupt
+  .FIT1_Toggle(FIT1_Toggle), // output FIT1_Toggle
+  .PIT1_Interrupt(PIT1_Interrupt), // output PIT1_Interrupt
+  .PIT1_Toggle(PIT1_Toggle), // output PIT1_Toggle
+  .GPO1(GPO1), // output [31 : 0] GPO1
+  .GPI1(GPI1), // input [31 : 0] GPI1
+  .GPI1_Interrupt(GPI1_Interrupt), // output GPI1_Interrupt
+  .INTC_Interrupt(INTC_Interrupt), // input [0 : 0] INTC_Interrupt
+  .INTC_IRQ(INTC_IRQ) // output INTC_IRQ
+);
+
+endmodule
+// You must compile the wrapper file mb_mcs.v when simulating
+// the core, mb_mcs. When compiling the wrapper file, be sure to
+// reference the XilinxCoreLib Verilog simulation library. For detailed
+// instructions, please refer to the "CORE Generator Help".
+
